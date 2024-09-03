e331223079153e3655ef7520fade3f7cd24d5241
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-09-03 22:44:21 +0600

    for loop decrement done

--- reverse.js
+++ reverse.js
@@ -12,4 +12,19 @@ for (const num of numbers) {
     // console.log(num);
     rev_numbers.unshift(num);
 }
-console.log(rev_numbers);
+// console.log(rev_numbers);
+
+const reversed_numbers = [];
+for (let i = 0; i < numbers.length; i++) {
+    const num = numbers[i];
+    reversed_numbers.unshift(num);
+}
+
+// decremental for loop
+const rev_rev_numbers = []; 
+for (let i = numbers.length -1 ; i >=0; i--){
+    const num = numbers[i];
+    console.log(num);
+    rev_rev_numbers.push(num);
+}
+console.log(rev_rev_numbers);
