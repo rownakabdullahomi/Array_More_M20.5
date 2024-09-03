ddaf3de95c2c6c33ef04509b93b03876eb9dd614
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-09-03 22:32:02 +0600

    reverse method done

---
+++ reverse.js
@@ -0,0 +1,7 @@
+//              [7, 6, 5, 4, 3, 2, 1]
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// console.log(numbers);
+const reversed = numbers.reverse();
+// console.log(reversed);
+// console.log(numbers); //Overwrites the original array
